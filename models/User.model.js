69ece0360f22a85531bcb560dbee8026e3335339
juan eugenio <leuviah77@gmail.com>
2023-08-01 16:32:10 +0200

    Reinstalled mongoose and modified new schema with mongoose.schema in user model

--- models/User.model.js
+++ models/User.model.js
@@ -1,35 +1,34 @@
 /** @format */
 
-const { Schema, model } = require("mongoose");
+const mongoose = require("mongoose");
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    // unique: true -> Ideally, should be unique, but its up to you
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-    default:
-      "https://res.cloudinary.com/dlfxinw9v/image/upload/v1631037631/default-profile-picture_sohcwq.png",
-  },
-  location: {
-    type: String,
-    required: false,
-  },
-  // socialMedia: { type: Array },         we will decide later on!!!
+const userSchema = new mongoose.Schema({
+	username: {
+		type: String,
+		required: true,
+		// unique: true -> Ideally, should be unique, but its up to you
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	image: {
+		type: String,
+		default: "https://res.cloudinary.com/dlfxinw9v/image/upload/v1631037631/default-profile-picture_sohcwq.png",
+	},
+	location: {
+		type: String,
+		required: false,
+	},
+	// socialMedia: { type: Array },         we will decide later on!!!
 });
 
-const User = model("User", userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
