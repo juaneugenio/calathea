f04f994cbfd5ce9ba3eda42f028c2d118cdc6a43
juan eugenio <leuviah77@gmail.com>
2023-08-01 16:49:03 +0200

    reinstalled dependency connect-mongo

--- utils/consts.js
+++ utils/consts.js
@@ -1,5 +1,5 @@
 /** @format */
 
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/calathea";
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost:3000/calathea";
 
 module.exports = MONGO_URI;
